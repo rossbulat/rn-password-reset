75376324eb585d7e07c5029c4511fb3b128893ee
Ross Bulat <ross@jkrbinvestments.com>
2020-01-13 17:47:21 +0800

    rm useEffect

--- ResetPassword/index.tsx
+++ ResetPassword/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { View, SafeAreaView } from 'react-native'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { Text } from '../Text'
